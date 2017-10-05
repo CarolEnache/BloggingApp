3a9962b4759ab98e53f153df5a6077cc656351fa
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-05 12:14:55 +0100

    Create post action creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import {reduxForm} from 'redux-form';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
     render(){
 
-        const {fields: {title, ctegories, content }, handleSubmit  } = this.props;
-        console.log(title);
+        const {fields: {title, categories, content }, handleSubmit  } = this.props;
 
         return (
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
                 <div className='form-group'>
                     <label>Title</label>
-                    <input type='text' className='form-control' {...title}/> 
+                    <input type='text' className='form-control' { ...title }/> 
                 </div>
                 <div className='form-group'>
                     <label>Categories</label>
-                    <input type='text' className='form-control' {...categories} /> 
+                    <input type='text' className='form-control' { ...categories } /> 
                 </div>
                 <div className='form-group'>
                     <label>Content</label>
-                    <textarea type='text' className='form-control' {...content} /> 
+                    <textarea type='text' className='form-control' { ...content } /> 
                 </div>
 
                 <button type='submit' className='btn btn-primary'>Submit</button>
@@ -29,7 +29,11 @@ class PostsNew extends Component {
     }
 }
 
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
-    from: 'PostsNewForm',
+    form: 'PostsNewForm',
     fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, {createPost})(PostsNew);
